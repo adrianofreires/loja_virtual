e25d01c6ef8672f0bf98afda2268ab14263582c2
Adriano Freires <freires.adriano@gmail.com>
2020-10-16 15:53:19 -0300

    até a seção do administrador
    Primeira vez commitado até a seção do administrador aula 60. Com alguns erros

---
+++ lib/screens/base/base_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/common/custom_drawer/custom_drawer.dart';
+import 'package:loja_virtual/models/page_manager.dart';
+import 'package:provider/provider.dart';
+import 'package:loja_virtual/screens/login/login_screen.dart';
+import 'package:loja_virtual/screens/product/products_screen.dart';
+import 'package:loja_virtual/screens/home/home_screen.dart';
+import 'package:loja_virtual/models/user_manager.dart';
+
+class BaseScreen extends StatelessWidget {
+  final PageController pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Provider(
+      create: (_) => PageManager(pageController),
+      child: Consumer<UserManager>(
+        builder: (_, userManager, __) {
+          return PageView(
+            controller: pageController,
+            physics: const NeverScrollableScrollPhysics(),
+            children: <Widget>[
+              HomeScreen(),
+              ProductsScreen(),
+              Scaffold(
+                drawer: CustomDrawer(),
+                appBar: AppBar(
+                  title: const Text('Home3'),
+                ),
+              ),
+              Scaffold(
+                drawer: CustomDrawer(),
+                appBar: AppBar(
+                  title: const Text('Home4'),
+                ),
+              ),
+              if (userManager.adminEnabled) ...[
+                Scaffold(
+                  drawer: CustomDrawer(),
+                  appBar: AppBar(
+                    title: const Text('Usuários'),
+                  ),
+                ),
+                Scaffold(
+                  drawer: CustomDrawer(),
+                  appBar: AppBar(
+                    title: const Text('Pedidos'),
+                  ),
+                ),
+              ]
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
